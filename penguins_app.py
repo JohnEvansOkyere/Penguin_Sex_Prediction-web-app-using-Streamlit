8d585599ca63f1fa20078a3675b214e753c6b635
JohnEvansOkyere <148287702+JohnEvansOkyere@users.noreply.github.com>
2024-09-12 15:37:53 +0000

    Fixed dependency issue in requirements.txt

--- penguins_app.py
+++ penguins_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
+import aiohttp
 from sklearn.ensemble import RandomForestClassifier
 
 # Set page configuration with title, icon, and layout
